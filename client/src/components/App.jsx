f77ce8f3d713eed339f84a9a3f107708238815a3
Kim T <kim200@kims-mbp.attlocal.net>
2019-07-23 03:05:54 -0700

    Render a list of all the tasks in the selected group

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,14 +1,43 @@
 import React, { Component } from 'react';
-
-
+import data from '../../../public/data.js';
+import ListTask from './ListTask.jsx';
 
 class App extends React.Component {
   constructor() {
-    super();
-  }
+		super();
+		
+		this.state = {
+			tasks: data,
+			isClicked: 0
+		}
+		this.handleClick = this.handleClick.bind(this);
+	}
+	
+	handleClick(e,value) {
+		e.preventDefault();
+		this.setState({
+			isClicked: value
+		})
+	}
+	
 	render() {
+		let click = this.state.isClicked;
+		let showTasks;
+
+		if (click === 1) {
+			showTasks = this.state.tasks.filter(task => task.group === 'Purchases');
+		}
+		if (click === 2) {
+			showTasks = this.state.tasks.filter(task => task.group === 'Build Airplane');
+		}
+		console.log('task:', showTasks)
 		return (
 			<div>
+				<h1>Things To Do</h1>
+					<p onClick={(e,value) => this.handleClick(e, 1)}>Task Group 1</p>
+					<p onClick={(e,value) => this.handleClick(e, 2)}>Task Group 2</p>
+			
+					{showTasks ? <ListTask showTask={showTasks} /> : null}
 				
 			</div>
 		)
--- server/index.js
+++ server/index.js
@@ -5,10 +5,10 @@ const path = require('path');
 const PORT = 3000;
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../client/dist/')));
 app.use(bodyParser.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname, '../client/dist/')));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
