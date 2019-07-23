cf492ce07100f4185f8b61f370b1ebc8b22ec509
Kim T <kim200@kims-mbp.attlocal.net>
2019-07-22 20:07:44 -0700

    Initialize the project

---
+++ client/src/components/App.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+
+
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+	render() {
+		return (
+			<div>
+				
+			</div>
+		)
+	}
+
+};
+
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/client');
+var DIST_DIR = path.join(__dirname, '/client/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  performance: {
+    hints: false
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              
+            },
+          },
+        ],
+      }
+    ]
+  }
+
+};
