32a45f8e4049b974799dba7322b4361ee4f40d17
Kim T <kim200@kims-mbp.attlocal.net>
2019-07-23 04:43:06 -0700

    Implement Overview screen

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,39 @@
 import React, { Component } from 'react';
 import data from '../../../public/data.js';
 import ListTask from './ListTask.jsx';
+import styled from 'styled-components';
+
+const Main = styled.div`
+	margin-left: 50px;
+	color: rgb(120, 120, 120);
+	font-family: medium-content-sans-serif-font, "Lucida Grande", "Lucida Sans Unicode", "Lucida Sans", Geneva, Arial, sans-serif;
+`;
+
+const Group = styled.h4`
+	display: inline-block;
+	margin-left: 30px;
+	margin-bottom: 1px;
+	margin-top: 40px;
+	color: rgb(0, 0, 0); 
+`;
+
+const Line = styled.div`
+	border: 1px solid rgb(180, 180, 180);
+	margin-right: 700px;
+	margin-top: 40px;
+`;
+
+const Status = styled.p`
+	margin-left: 40px;
+	margin-top: 1px;
+	color: rgb(180, 180, 180);
+	font-family: medium-content-sans-serif-font, "Lucida Grande", "Lucida Sans Unicode", "Lucida Sans", Geneva, Arial, sans-serif;
+`;
+
+const Icon = styled.i`
+	display: inline-flex;
+	vertical-align: -9px; 
+`;
 
 class App extends React.Component {
   constructor() {
@@ -26,18 +59,24 @@ class App extends React.Component {
 
 		if (click === 0) {
 			showTasks = 
-				<div>
+				<Main>
 					<h1>Things To Do</h1>
-					<p onClick={(e,value) => this.handleClick(e, 1)}>Task Group 1</p>
-					<p onClick={(e,value) => this.handleClick(e, 2)}>Task Group 2</p>
-				</div>
+					<Line></Line>
+						<Icon className="fas fa-caret-right"></Icon> <Group onClick={(e,value) => this.handleClick(e, 1)}>Task Group 1</Group>
+						<Status>1 OF 3 TASKS COMPLETE</Status>
+					<Line></Line>
+						<Icon className="fas fa-caret-right"></Icon> <Group onClick={(e,value) => this.handleClick(e, 2)}>Task Group 2</Group>
+						<Status>0 OF 2 TASKS COMPLETE</Status>
+					<Line></Line>
+
+				</Main>
 		};
 
 		if (click === 1) {
 			showTasks = 
 				<div>
 					<h1>Task Group 1</h1>
-					<ListTask showTask={this.state.tasks.filter(task => task.group === 'Purchases')}/>
+					<ListTask showTask={this.state.tasks.filter(task => task.group === 'Purchases')}/>	
 				</div>
 		};
 
@@ -45,7 +84,7 @@ class App extends React.Component {
 			showTasks = 
 			<div>
 				<h1>Task Group 2</h1>
-				<ListTask showTask={this.state.tasks.filter(task => task.group === 'Build Airplane')}/>
+				<ListTask showTask={this.state.tasks.filter(task => task.group === 'Build Airplane')}/>				
 			</div>
 		};
 	
