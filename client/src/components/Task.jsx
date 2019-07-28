bd69d298ffadff3ba60b77bbb624a9ce808bdb87
Kim T <kim200@kims-mbp.attlocal.net>
2019-07-27 19:02:55 -0700

    Implement checkbox style

--- client/src/components/Task.jsx
+++ client/src/components/Task.jsx
@@ -2,34 +2,66 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 
 
-const Group = styled.h4`
-	display: inline-block;
-	margin-left: 30px;
-	margin-top: 30px;
-	color: rgb(0, 0, 0); 	
+const Label = styled.label`
+  flex-shrink: 0;
+  padding: .5rem 1rem;
+  color: rgb(0, 0, 0); 	
+  cursor: pointer;
+  font-size: 16px;
+  font-weight: bold;
+  font-family: 'system-ui';
+`;
+
+const Box = styled.input`
+  position: relative;
+  flex-shrink: 0;
+  width: 20px;
+  height: 20px;
+  appearance: none;
+  outline: none; 
+  background: white;
+  cursor: pointer;
+  border: 1px solid rgb(180, 180, 180);
+  &::before {
+    content: ' ';
+    position: absolute;
+    top: 50%;
+    right: 50%;
+    bottom: 50%;
+    left: 50%;
+    transition: all .1s;
+    background: #00ff99;
+  }
+  &:checked{
+    &::before {
+      top: 2px;
+      right: 2px;
+      bottom: 2px;
+      left: 2px;
+    }
+  }
 `;
 
 const Line = styled.div`
 	border: 1px solid rgb(180, 180, 180);
-	width: 500px;
-	margin-top: 20px;
+  width: 500px;
+	margin-top: 30px;
 `;
 
 const Container = styled.div`
-	display: block;
-	height: 70px;
+	display: flex;
+  align-items: center;
+  margin: 20px;
 `;
 
-const Icon = styled.i`
-	display: inline-flex;
-`;
 
 class Task extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-			checked: false,
+      checked: false,
+      lockedTask: undefined,
 		}
 		this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
 		
@@ -39,7 +71,13 @@ class Task extends React.Component {
 			this.setState({ 
 			checked: !this.state.checked,
 		})
-	}
+  }
+  
+  // componentDidUpdate(prevProps) {
+  //   if (prevProps.indeterminate !== this.props.indeterminate) {
+  //     this.selector.indeterminate = this.props.indeterminate;
+  //   }
+  // }
 
 
   render() {
@@ -49,8 +87,8 @@ class Task extends React.Component {
     return (
       <div >
 		 		<Container>
-		 			<input type="checkbox" onChange={this.handleCheckboxChange} />
-		 			<Group >{text}</Group> 
+		 			<Box type="checkbox" onChange={this.handleCheckboxChange} />
+		 			<Label >{text}</Label> 
 		 		</Container>
 		 		<Line></Line>
 		 	</div>
