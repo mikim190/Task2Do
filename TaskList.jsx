7fa5196d4e6cd31f68b21ef3ef9b8fe78fd8de73
Kim Tran <mykim.tran297@gmail.com>
2019-08-29 02:53:15 -0700

    Clean up extra code for styling Task comp

---
+++ TaskList.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import Task from "./Task.jsx";
+import styled from "styled-components";
+
+const Main = styled.div`
+  margin-left: 10px;
+  color: rgb(120, 120, 120);
+  font-family: "system-ui";
+`;
+
+const Line = styled.div`
+  border: 1px solid rgb(180, 180, 180);
+  width: 500px;
+  margin-top: 20px;
+`;
+
+class TaskList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const data = this.props.showTask;
+
+    return (
+      <Main>
+        <Line></Line>
+        {data.map((value, id, allTasks) => (
+          <Task
+            key={value.id}
+            data={value}
+            allData={allTasks}
+            finTaskIds={this.props.finTaskIds}
+            updateRecord={this.props.updateRecord}
+          />
+        ))}
+      </Main>
+    );
+  }
+}
+
+export default TaskList;
